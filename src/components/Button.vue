dc1c5ce2e719fac8468b244b2f8fa18ac479d89f
Leen4k <110277798+Leen4k@users.noreply.github.com>
2023-10-21 20:45:13 +0700

    TP01 internet programming: My first vue js hello world make component pass props etc...

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button :class="background+ ` flex gap-2 text-xs text-white justify-center items-center rounded-md w-48rem p-2`">
+      {{ text }}
+      <arrow-long-right-icon class="icon w-4 h-4"/>
+    </button>
+  </template>
+  
+  <script>
+  import {ArrowLongRightIcon} from '@heroicons/vue/20/solid';
+  export default {
+    name: "Button",
+    props: {
+      text: String, // Define a prop to accept the button text
+      background: String,
+    },
+    components: { ArrowLongRightIcon }
+  };
+  </script>
---
+++ src/components/Promotion.vue
@@ -0,0 +1,32 @@
+<script>
+import Button from './Button.vue'
+export default {
+    name: "Promotion",
+    props: {
+        name: String,
+        bgButton: String, // Change the prop name to "background"
+        imageUrl: String,
+        buttonText: String
+    },
+    components: { Button }
+}
+</script>
+
+<template>
+    <div class="flex justify-center h-[220px] items-center w-[320px] rounded-md shadow-md overflow-hidden">
+        <!-- <slot></slot> -->
+        <div class="flex flex-col m-auto flex-1 px-4 gap-4">
+          <div class="flex items-center">
+            <h3 class="">{{ name }}</h3>
+          </div>
+          <Button :background="bgButton" :text="buttonText" />
+        </div>
+
+        <div class="relative h-full w-1/2 overflow-visible">
+          <img class="h-full absolute w-full object-cover top-5 bottom-5" :src="imageUrl" :alt="name" />
+        </div>
+    </div>
+</template>
+
+
+
