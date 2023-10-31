eddf15e838d624fc7f72009b54cdc4623bead57a
Leen4k <110277798+Leen4k@users.noreply.github.com>
2023-10-31 17:34:42 +0700

    pass icon as props

--- src/components/Promotion.vue
+++ src/components/Promotion.vue
@@ -1,32 +1,35 @@
 <script>
-import Button from './Button.vue'
+import Button from './Button.vue';
+import { ArrowLongRightIcon } from '@heroicons/vue/20/solid';
+
 export default {
-    name: "Promotion",
-    props: {
-        name: String,
-        bgButton: String, // Change the prop name to "background"
-        imageUrl: String,
-        buttonText: String
-    },
-    components: { Button }
+  name: "Promotion",
+  props: {
+    name: String,
+    bgButton: String,
+    imageUrl: String,
+    buttonText: String
+  },
+  components: { Button },
+  data() {
+    return {
+      iconComponent: ArrowLongRightIcon, // Pass the icon component as a data property
+    };
+  }
 }
 </script>
 
 <template>
-    <div class="flex justify-center h-[220px] items-center w-[320px] rounded-md shadow-md overflow-hidden">
-        <!-- <slot></slot> -->
-        <div class="flex flex-col m-auto flex-1 px-4 gap-4">
-          <div class="flex items-center">
-            <h3 class="">{{ name }}</h3>
-          </div>
-          <Button :background="bgButton" :text="buttonText" />
-        </div>
-
-        <div class="relative h-full w-1/2 overflow-visible">
-          <img class="h-full absolute w-full object-cover top-5 bottom-5" :src="imageUrl" :alt="name" />
-        </div>
+  <div class="flex justify-center h-[220px] items-center w-[320px] rounded-md shadow-md overflow-hidden">
+    <div class="flex flex-col m-auto flex-1 px-4 gap-4">
+      <div class="flex items-center">
+        <h3>{{ name }}</h3>
+      </div>
+      <Button :background="bgButton" :text="buttonText" :iconComponent="iconComponent" />
     </div>
+
+    <div class="relative h-full w-1/2 overflow-visible">
+      <img class="h-full absolute w-full object-cover top-5 bottom-5" :src="imageUrl" :alt="name" />
+    </div>
+  </div>
 </template>
-
-
-
