50a79ae266f58b63e0f0a1abcbc22b7aa4d93e83
Leen4k <110277798+Leen4k@users.noreply.github.com>
2023-12-01 08:15:56 +0700

    completed tp3

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,9 @@
 <template>
   <button :class="background +` `+ textColor + ` flex gap-2 text-xs justify-center items-center rounded-md w-48rem p-2`">
-    {{ text }}
+    <div class="flex items-center gap-1">
+      {{ text  }}   
+      <component :is="optionalIcon" class="icon w-4 h-4 order-last" /> 
+    </div>
     <component :is="iconComponent" class="icon w-4 h-4" />
   </button>
 </template>
@@ -13,6 +16,10 @@ export default {
     background: String,
     textColor: String,
     iconComponent: Object, // Define the prop type as Object for a component
+    optionalIcon: {
+      type: Object,
+      default: null
+    },
   },
 };
 </script>
